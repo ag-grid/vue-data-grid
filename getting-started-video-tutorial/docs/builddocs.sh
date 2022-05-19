794f8eb90bfd240944d7eafe0dad028f8881b421
Alan Richardson <alan.richardson@ag-grid.com>
2022-05-19 15:00:31 +0100

    enterprise enablement

--- getting-started-video-tutorial/docs/builddocs.sh
+++ getting-started-video-tutorial/docs/builddocs.sh
@@ -6,6 +6,9 @@ cat newpage.md >> vue-tutorial.md
 cat 001-quickstart-vue-ag-grid.md >> vue-tutorial.md
 
 cat newpage.md >> vue-tutorial.md
+cat 002-enable-enterprise.md >> vue-tutorial.md
+
+cat newpage.md >> vue-tutorial.md
 cat 999-outro.md >> vue-tutorial.md
 
 pandoc vue-tutorial.md -o vue-tutorial-workbook.pdf --from markdown --template eisvogel --listings --toc
